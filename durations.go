96579829e5825c30c731f4af44fd0832846563de
henderjon <list@box370.net>
2019-04-16 11:25:26 -0500

    adds comments and Weekof

--- durations.go
+++ durations.go
@@ -1,48 +1,69 @@
 package durations
 
-import "time"
+import (
+	"time"
+)
 
-type Runner func()
-
-func NewRunner(interval time.Duration, runners []Runner) {
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			for _, runner := range runners {
-				runner()
-			}
-		}
-	}
-}
-
+// Nanoseconds returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Nanoseconds(n int) time.Duration {
 	// 1
 	return time.Duration(n) * time.Nanosecond
 }
 
+// Microseconds returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Microseconds(n int) time.Duration {
 	// 1000 * Nanosecond
 	return time.Duration(n) * time.Microsecond
 }
 
+// Milliseconds returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Milliseconds(n int) time.Duration {
 	// 1000 * Microsecond
 	return time.Duration(n) * time.Millisecond
 }
 
+// Seconds returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Seconds(n int) time.Duration {
 	// 1000 * Millisecond
 	return time.Duration(n) * time.Second
 }
 
+// Minutes returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Minutes(n int) time.Duration {
 	// 60 * Second
 	return time.Duration(n) * time.Minute
 }
 
+// Hours returns a duration based on ints. time.Duration is an int64, but there are
+// times when the return value of funcs or other operations aren't int64. This is
+// pure sugar for casting an int to a time.Duration before doing the math.
 func Hours(n int) time.Duration {
 	// 60 * Minute
 	return time.Duration(n) * time.Hour
+}
+
+// Weekof returns the date of the Monday of the week of the date provided. This is useful
+// for communicating the date of the week when using ISO 8601 weeks.
+// read more at https://golang.org/pkg/time/#Time.ISOWeek
+func Weekof(date time.Time) time.Time {
+
+	for int(date.Weekday()) > int(time.Monday) {
+		date = date.Add(-time.Hour)
+	}
+
+	for int(date.Weekday()) < int(time.Sunday) {
+		date = date.Add(time.Hour)
+	}
+
+	return date
 }
--- durations_test.go
+++ durations_test.go
@@ -1,31 +1,55 @@
 package durations
 
 import (
-	"strings"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestInterval(t *testing.T) {
+func TestDurations(t *testing.T) {
 
-	mockPingChan := make(chan string)
+	real := Hours(1)
 
-	go NewRunner(Seconds(3), []Runner{
-		func() {
-			mockPingChan <- "BOOM"
-			close(mockPingChan)
-		},
-	})
-
-	var mockPingResult strings.Builder
-	for s := range mockPingChan {
-		// log.Println("interval")
-		mockPingResult.WriteString(s)
+	if diff := cmp.Diff(int64(3600000000000), real.Nanoseconds()); diff != "" {
+		t.Errorf("Nanoseconds: (+want/-got)\n%s", diff)
 	}
 
-	if diff := cmp.Diff(mockPingResult.String(), "BOOM"); diff != "" {
-		t.Errorf("cron: (-got +want)\n%s", diff)
+	if diff := cmp.Diff(float64(3600), real.Seconds()); diff != "" {
+		t.Errorf("Seconds: (+want/-got)\n%s", diff)
+	}
+
+	if diff := cmp.Diff(float64(60), real.Minutes()); diff != "" {
+		t.Errorf("Minutes: (+want/-got)\n%s", diff)
+	}
+
+	if diff := cmp.Diff(float64(1), real.Hours()); diff != "" {
+		t.Errorf("Hours: (+want/-got)\n%s", diff)
+	}
+
+}
+
+func TestWeekof(t *testing.T) {
+
+	// notice that the date is the 31st of Dec, 2014, but the Monday of that date's week
+	// is the 29th but those dates are considered the first week of 2015.
+	ts, e := time.Parse("2006-01-02", "2014-12-31")
+	if e != nil {
+		t.Errorf(e.Error())
+	}
+
+	ts = Weekof(ts)
+
+	if diff := cmp.Diff("2014-12-29 Monday", ts.Format("2006-01-02 Monday")); diff != "" {
+		t.Errorf("Format: (+want/-got)\n%s", diff)
+	}
+
+	y, w := ts.ISOWeek()
+	got := fmt.Sprintf("%d%02d", y, w)
+
+	if diff := cmp.Diff("201501", got); diff != "" {
+		t.Errorf("year: (+want/-got)\n%s", diff)
 	}
 
 }
